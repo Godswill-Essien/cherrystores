071d1efe7cb62f3a9d26e97311c021f5d83552ef
unknown <epellecherrypick@gmail.com>
2025-01-19 04:03:55 +0100

    new arrvals

---
+++ src/app/New/page.jsx
@@ -0,0 +1,10 @@
+import Newarrival from '@/components/Newarrival'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='bg-white text-black'>
+      <Newarrival/>
+    </div>
+  )
+}
--- src/components/Newarrival.jsx
+++ src/components/Newarrival.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function newarrival() {
+export default function Newarrival() {
   return (
     <div>
-      <h1>new arrival</h1>
+      <h1>FEATURED PRODUCTS</h1>
+      <p>we have your occasion covered</p>
+      <div className="featured flex w-full flex-wrap  justify-evenly ">
+        <img className='h-[350px] w-[250px] pb-5' src="Images/mybrownhat.jpg" alt="" />
+        <img className='h-[350px] w-[250px] pb-5' src="Images/mysuit.jpg" alt="" />
+        <img className='h-[350px] w-[250px] pb-5' src="Images/mypinkshirt.jpg" alt="" />
+        <img className='h-[350px] w-[250px] pb-5' src="Images/myblueshirt.jpg" alt="" />
+        <img className='h-[350px] w-[250px]' src="Images/myblackdress.jpg" alt="" />
+        <img className='h-[350px] w-[250px]' src="Images/myjeandress.jpg" alt="" />
+        <img className='h-[350px] w-[250px]' src="Images/myblackhat.jpg" alt="" />
+        <img className='h-[350px] w-[250px]' src="Images/mypinksweatshirt.jpg" alt="" />
+      </div>
     </div>
   )
 }
