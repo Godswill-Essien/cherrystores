117c2cc6a4e1ff4f4e2637944edc7d3c115337dd
Godswill-Essien <godswillessien880@gmail.com>
2025-01-20 12:36:56 +0100

    singleproduct part 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'rivon-demo.myshopify.com',
+            
+          },
+        ],
+      },
+};
 
 export default nextConfig;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Navbar from '@/components/Navbar'
+import Singleproduct from '@/components/Singleproduct'
 import React from 'react'
 
 export default function page() {
   return (
 
-    <div>hi my name is cherrypick am from sokoto walai i handsome
-      <Navbar/>
-       </div>
+    <div>
+      <Singleproduct />
+      
+    </div>
   )
 }
